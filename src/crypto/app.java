0cea632926c0e0d4ae44aaa4567f2e6decb8a15e
jwx <kamalpandey1x@gmail.com>
2018-05-05 19:19:23 +0530

    encryption/decryption up

--- src/crypto/app.java
+++ src/crypto/app.java
@@ -7,28 +7,67 @@ package crypto;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import javax.swing.*;
 import javax.swing.GroupLayout;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
 
 /**
  * @author Kamal Pandey
  */
 public class app extends JPanel {
+
+    private JFileChooser fileChooser;
+
+    File file;
+
     public app() {
         initComponents();
 
-        button_upload.addActionListener(new ActionListener() {
+        fileChooser = new JFileChooser();
+
+        button_browse.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null, "I am awesome!");
+                browseButtonActionPerformed(e);
             }
         });
+
+        button_encrypt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                encryptButtonAction(e);
+            }
+        });
+
+        button_decrypt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                decryptButtonAction(e);
+            }
+        });
+
+
+
     }
+
 
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
         // Generated using JFormDesigner Evaluation license - Kamal Pandey
-        button_upload = new JButton();
+        button_browse = new JButton();
         textField_key1 = new JTextField();
         button_encrypt = new JButton();
         label1 = new JLabel();
@@ -36,19 +75,20 @@ public class app extends JPanel {
         textField_key2 = new JTextField();
         button_decrypt = new JButton();
         label3 = new JLabel();
+        fileTextField = new JTextField();
 
         //======== this ========
 
         // JFormDesigner evaluation mark
         setBorder(new javax.swing.border.CompoundBorder(
             new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(0, 0, 0, 0),
-                "JWX", javax.swing.border.TitledBorder.CENTER,
+                "JFormDesigner Evaluation", javax.swing.border.TitledBorder.CENTER,
                 javax.swing.border.TitledBorder.BOTTOM, new java.awt.Font("Dialog", java.awt.Font.BOLD, 12),
                 java.awt.Color.red), getBorder())); addPropertyChangeListener(new java.beans.PropertyChangeListener(){public void propertyChange(java.beans.PropertyChangeEvent e){if("border".equals(e.getPropertyName()))throw new RuntimeException();}});
 
 
-        //---- button_upload ----
-        button_upload.setText("upload");
+        //---- button_browse ----
+        button_browse.setText("browse");
 
         //---- button_encrypt ----
         button_encrypt.setText("encrypt");
@@ -74,21 +114,24 @@ public class app extends JPanel {
                     .addGap(35, 35, 35)
                     .addGroup(layout.createParallelGroup()
                         .addComponent(button_decrypt)
-                        .addComponent(button_upload)
-                        .addComponent(button_encrypt)
+                        .addGroup(layout.createSequentialGroup()
+                            .addComponent(button_browse)
+                            .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                            .addComponent(fileTextField, GroupLayout.PREFERRED_SIZE, 101, GroupLayout.PREFERRED_SIZE))
                         .addGroup(layout.createSequentialGroup()
                             .addGap(8, 8, 8)
                             .addComponent(label2)
-                            .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+                            .addGap(18, 18, 18)
                             .addComponent(textField_key2, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE))
                         .addGroup(layout.createSequentialGroup()
                             .addGap(6, 6, 6)
                             .addComponent(label1)
-                            .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+                            .addGap(18, 18, 18)
                             .addGroup(layout.createParallelGroup()
-                                .addComponent(textField_key1, GroupLayout.PREFERRED_SIZE, 102, GroupLayout.PREFERRED_SIZE)
-                                .addComponent(label3))))
-                    .addContainerGap(105, Short.MAX_VALUE))
+                                .addComponent(label3)
+                                .addComponent(textField_key1, GroupLayout.PREFERRED_SIZE, 102, GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(button_encrypt))
+                    .addContainerGap(99, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup()
@@ -96,7 +139,9 @@ public class app extends JPanel {
                     .addContainerGap()
                     .addComponent(label3)
                     .addGap(8, 8, 8)
-                    .addComponent(button_upload)
+                    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                        .addComponent(button_browse)
+                        .addComponent(fileTextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
                     .addGap(10, 10, 10)
                     .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
                         .addComponent(label1)
@@ -116,7 +161,7 @@ public class app extends JPanel {
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
     // Generated using JFormDesigner Evaluation license - Kamal Pandey
-    private JButton button_upload;
+    private JButton button_browse;
     private JTextField textField_key1;
     private JButton button_encrypt;
     private JLabel label1;
@@ -124,7 +169,50 @@ public class app extends JPanel {
     private JTextField textField_key2;
     private JButton button_decrypt;
     private JLabel label3;
+    private JTextField fileTextField;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
+
+    private void browseButtonActionPerformed(ActionEvent e) {
+        // file chooser to select image file
+        int fileChoose = fileChooser.showOpenDialog(this);
+        if (fileChoose == JFileChooser.APPROVE_OPTION)
+        {
+            file = fileChooser.getSelectedFile();
+
+            // display file path in text field
+            fileTextField.setText(file.getAbsolutePath());
+
+        }
+    }
+
+    private void encryptButtonAction(ActionEvent e){
+        String key = "qwe rtgfd qwertf";
+        File encryptedFile = new File("/home/jwx/Desktop/encrypted.jpg");
+
+
+
+        try {
+            Crypto.encrypt(key, file, encryptedFile);
+        } catch (CryptoException ex) {
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+        }
+
+    }
+
+    private void decryptButtonAction(ActionEvent e){
+        String key = "qwe rtgfd qwertf";
+        File encryptedFile = new File("/home/jwx/Desktop/encrypted.jpg");
+        File decryptedFile = new File("/home/jwx/Desktop/decrypted.jpg");
+
+        try {
+            Crypto.decrypt(key, encryptedFile, decryptedFile);
+        } catch (CryptoException ex) {
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("app");
@@ -132,5 +220,57 @@ public class app extends JPanel {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+    }
+}
+
+
+class Crypto {
+    private static final String ALGORITHM = "AES";
+    private static final String TRANSFORMATION = "AES";
+
+    public static void encrypt(String key, File inputFile, File outputFile)
+            throws CryptoException {
+        doCrypto(Cipher.ENCRYPT_MODE, key, inputFile, outputFile);
+    }
+
+    public static void decrypt(String key, File inputFile, File outputFile)
+            throws CryptoException {
+        doCrypto(Cipher.DECRYPT_MODE, key, inputFile, outputFile);
+    }
+
+    private static void doCrypto(int cipherMode, String key, File inputFile,
+                                 File outputFile) throws CryptoException {
+        try {
+            Key secretKey = new SecretKeySpec(key.getBytes(), ALGORITHM);
+            Cipher cipher = Cipher.getInstance(TRANSFORMATION);
+            cipher.init(cipherMode, secretKey);
+
+            FileInputStream inputStream = new FileInputStream(inputFile);
+            byte[] inputBytes = new byte[(int) inputFile.length()];
+            inputStream.read(inputBytes);
+
+            byte[] outputBytes = cipher.doFinal(inputBytes);
+
+            FileOutputStream outputStream = new FileOutputStream(outputFile);
+            outputStream.write(outputBytes);
+
+            inputStream.close();
+            outputStream.close();
+
+        } catch (NoSuchPaddingException | NoSuchAlgorithmException
+                | InvalidKeyException | BadPaddingException
+                | IllegalBlockSizeException | IOException ex) {
+            throw new CryptoException("Error encrypting/decrypting file", ex);
+        }
+    }
+}
+
+class CryptoException extends Exception {
+
+    public CryptoException() {
+    }
+
+    public CryptoException(String message, Throwable throwable) {
+        super(message, throwable);
     }
 }
